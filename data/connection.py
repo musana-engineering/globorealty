7dbc5f063bb6135af66d73f3dc5a0da9d104d3c4
musana.engineering@aol.com <musana.engineering@aol.com>
2025-09-06 23:52:07 +0000

    Adding Snowflake Connection

--- data/connection.py
+++ data/connection.py
@@ -19,7 +19,6 @@ subscription_id = os.getenv("SUBSCRIPTION_ID")
 resource_group_name = os.getenv("RESOURCE_GROUP_NAME")
 workspace_name = os.getenv("WORKSPACE_NAME")
 
-
 ml_client = MLClient(
     DefaultAzureCredential(),
     subscription_id=subscription_id,
@@ -28,16 +27,10 @@ ml_client = MLClient(
 )
 
 # Verify the ML Client connection is succesful by querying the workspace
-workspace_details = ml_client.workspaces.get(
-    name=workspace_name
-)
-
+workspace_details = ml_client.workspaces.get(name=workspace_name)
 print(json.dumps(workspace_details._to_dict(), indent=4))
 
 import urllib.parse
-username = urllib.parse.quote(os.environ["SNOWFLAKEDB_USERNAME"], safe="")
-password = urllib.parse.quote(os.environ["SNOWFLAKEDB_PASSWORD"], safe="")
-
 snowflake_account = os.getenv("SNOWFLAKE_ACCOUNT")
 snowflake_database = os.getenv("SNOWFLAKE_DATABASE")
 snowflake_warehouse = os.getenv("SNOWFLAKE_WAREHOUSE")
