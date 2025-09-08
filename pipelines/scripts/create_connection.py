a97c70166d1a82bc5be72b54d91a16e6c09bdb4b
musana.engineering@aol.com <musana.engineering@aol.com>
2025-09-08 03:12:36 +0000

    Adding Data Scripts

--- pipelines/scripts/create_connection.py
+++ pipelines/scripts/create_connection.py
@@ -22,7 +22,7 @@ connection_name = "Snowflake"
 # Create the connection
 try:
     ml_client.connections.get(name=connection_name)
-    print("Connection with the same name already exists")
+    print(f"Connection '{connection_name}' already exists")
 except:
     wps_connection = WorkspaceConnection(
         name=connection_name,
@@ -31,7 +31,7 @@ except:
         credentials=UsernamePasswordConfiguration(username=snowflake_db_username, password=snowflake_db_password)
     )
     ml_client.connections.create_or_update(workspace_connection=wps_connection)
-    print("Workspace connection created.")
+    print(f"Workspace connection '{connection_name}' created.")
 
 # Return list of connections to verify the new connection exists
 verify_connection_creation = ml_client.connections.list(connection_type="snowflake")
